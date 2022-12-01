1e22e89b3bf881e50abfe15cecd63117939295fd
Netology-cp <103128444+Netology-cp@users.noreply.github.com>
2022-12-02 01:49:25 +0300

    Update Main.java

--- 01/Main.java
+++ 01/Main.java
@@ -2,50 +2,40 @@ import java.util.Scanner;
 
 public class Main {
 
-    static String word = "javalove";
+    static String word = "javalove"; // слово, которое загадано
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        // javalove --------
         System.out.println("Угадай слово");
         int lengthWord = word.length();
-        String maskWord = "-".repeat(lengthWord);
+        String maskWord = "-".repeat(lengthWord); // в начале вместо каждой буквы в слове стоит знак «-», если буква будет угдана, то знак поменяется на нужную букву
         System.out.println(maskWord);
 
-        // --------
-        // a
-        // -a-a----
-        // o
-        // -a-a-o--
         do {
-            System.out.println("Введите буква");
-            char c = input.next().charAt(0);
-            // a 2
-            // w -1
-            if (word.indexOf(c) >= 0) {
+            System.out.println("Введите букву");
+            char c = input.next().charAt(0); // считываем введенный символ
+            
+            if (word.indexOf(c) >= 0) { // проверяем есть ли введенная буква в слове
                 System.out.println("Удача");
-                for (char elem : word.toCharArray()) {
+                for (char elem : word.toCharArray()) { // просматриваем все слово, чтобы заменить «-» на буквы
                     if (elem == c) {
-                        maskWord = replaceMaskLetter(c, maskWord);
+                        maskWord = replaceMaskLetter(c, maskWord); // если буква есть, то вызываем метод replaceMaskLetter, который заменяет «-» в слове на найденную букву
                     }
                 }
-                System.out.println(maskWord);
+                System.out.println(maskWord); // выводим обновленное слово с добавленной отгаданной буквой
             } else {
                 System.out.println("Промах, поробуй еще раз");
-                System.out.println(maskWord);
+                System.out.println(maskWord); // если буквы нет, то просто выводим слово с «-» и ранее угаданными буквами
             }
-        } while (maskWord.contains("-"));
+        } while (maskWord.contains("-")); // повторяем действия пока в слове еще будут неотгаданные буквы, то есть будет хотя бы один символ «-»
         System.out.println("Поздравляем ты выйграл");
     }
 
-    /*
-    -a-a---- j
-    ja-a----
-     */
-    public static String replaceMaskLetter(char c, String maskWord) {
+
+    public static String replaceMaskLetter(char c, String maskWord) { // метод, который в строке maskWord заменяет «-» на символ «с» в соответствии с загаданным словом
         StringBuilder stringBuilder = new StringBuilder();
-        for (int i = 0; i < word.length(); i++) {
+        for (int i = 0; i < word.length(); i++) { // в цикле по порядку перебираем каждую букву из слова
             if (word.charAt(i) == c) {
                 stringBuilder.append(c);
             } else if (maskWord.charAt(i) != '-') {
