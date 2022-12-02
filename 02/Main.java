8e23067cc1ef8a109bddadb3eb489e4c0555e700
Netology-cp <103128444+Netology-cp@users.noreply.github.com>
2022-12-02 17:07:22 +0300

    Update Main.java

--- 02/Main.java
+++ 02/Main.java
@@ -3,84 +3,54 @@ import java.util.concurrent.TimeUnit;
 
 
 public class Main {
+    public static final int WORK = 1;
+    public static final int BREAK = 1;
 
+    public static void main(String[] args) throws InterruptedException {
 
-    public static int test=0;
-    public static void main(String[] args) throws InterruptedException{
-        
-        String[] cmd = new Scanner(System.in).nextLine().split(" ");
+        System.out.println("Привет! Я тайм-менеджер Pomodoro!\nВведите количество задач на сегодня:");
 
-        int work = 50;
-        int breake = 10;
-        int sizebreak = 30;
-        int sizework = 30;
-        int help = 0;
-        int count = 1;
+        int count = new Scanner(System.in).nextInt();
 
-        for(int i=0; i < cmd.length;i++){
-            switch (cmd[i]) {
-                case "--help" -> {
-                    System.out.println(
-                            "\n\nPomodoro - сделай свое время более эффективным\n");
-                    System.out.println(
-                            "	-w <time>: время работы, сколько хочешь работать.\n");
-                    System.out.println(
-                            "	-b <time>: время отдыха, сколько хочешь отдыхать.\n");
-                    System.out.println(
-                            "	-count <count>: количество итераций.\n");
-                    System.out.println(
-                            "	--help: меню помощи.\n");
-                    help = 1;
-                }
-                case "-w" -> work = Integer.parseInt(cmd[++i]);
-                case "-b" -> breake = Integer.parseInt(cmd[++i]);
-                case "-count" -> count = Integer.parseInt(cmd[++i]);
-            }
+        long startTime = System.currentTimeMillis();
+        for (int i = 1; i <= count; i++) {
+            timer(i);
         }
-        if(help == 0){
-            long startTime = System.currentTimeMillis();
-            for (int i = 1; i <= count; i++) {
-                timer(work, breake, sizebreak, sizework);
-            }
-            long endTime = System.currentTimeMillis();
-            System.out.println("Pomodor таймер истек: " + (endTime-startTime)/(1000 * 60)+ " min");
-        }
-
+        long endTime = System.currentTimeMillis();
+        System.out.println("Pomodoro таймер истек: " + (endTime - startTime) / (1000 * 60) + " min");
     }
 
-    public static void timer(int work, int breake, int sizebreak, int sizework) throws InterruptedException{
-
-        printProgress("Work Progress::  ", work, sizework);
-
-        printProgress("Break Progress:: ", breake, sizebreak);
+    public static void timer(int count) throws InterruptedException {
+        System.out.println("Задача №" + count);
+        printProgress("Work Progress::  ", WORK);
+        printProgress("Break Progress:: ", BREAK);
     }
 
-    private static void printProgress(String process, int time, int size) throws InterruptedException {
+    private static void printProgress(String process, int time) throws InterruptedException {
         int length;
         int rep;
-        length = 60* time / size;
-        rep = 60* time /length;
-        int stretchb = size /(3* time);
-        for(int i=1; i <= rep; i++){
+        length = 60 * time / 30;
+        rep = 60 * time / length;
+        int stretch = 30 / (3 * time);
+        for (int i = 1; i <= rep; i++) {
             double x = i;
-            x = 1.0/3.0 *x;
+            x = 1.0 / 3.0 * x;
             x *= 10;
             x = Math.round(x);
             x /= 10;
-            double w = time *stretchb;
-            double percent = (x/w) *1000;
-            x /=stretchb;
+            double w = time * stretch;
+            double percent = (x / w) * 1000;
+            x /= stretch;
             x *= 10;
             x = Math.round(x);
             x /= 10;
             percent = Math.round(percent);
             percent /= 10;
-            System.out.print(process + percent+"% " + (" ").repeat(5 - (String.valueOf(percent).length())) +"[" + ("#").repeat(i) + ("-").repeat(rep - i)+"]    ( " + x +"min / " + time +"min )"+  "\r");
-            if(test == 0){
-                TimeUnit.SECONDS.sleep(length);
-            }
+            System.out.print(process + percent + "% " + (" ")
+                    .repeat(5 - (String.valueOf(percent).length())) + "[" + ("#")
+                    .repeat(i) + ("-").repeat(rep - i) + "]    ( " + x + "min / " + time + "min )" + "\r");
+            TimeUnit.SECONDS.sleep(length);
         }
         System.out.println();
     }
 }
-
