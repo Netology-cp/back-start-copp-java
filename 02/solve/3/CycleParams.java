7043b5f32622306c6b51b715a16c072baa1410bf
Netology-cp <103128444+Netology-cp@users.noreply.github.com>
2022-12-02 18:10:32 +0300

    Add files via upload

---
+++ 02/solve/3/CycleParams.java
@@ -0,0 +1,31 @@
+﻿package org.example.pomodoro;
+
+public class CycleParams {
+    private int work = 50;
+    private int breake = 10;
+    private int count = 1;
+
+    public int getWork() {
+        return work;
+    }
+
+    public void setWork(int work) {
+        this.work = work;
+    }
+
+    public int getBreake() {
+        return breake;
+    }
+
+    public void setBreake(int breake) {
+        this.breake = breake;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+}
